42687f002903d5b87029c2bceca51785f14a11ec
KochevDmitry <65921394+KochevDmitry@users.noreply.github.com>
2023-07-08 19:10:04 +0300

    Update update_coords.cpp
    without using namespace std

--- send_coordinates/update_coords.cpp
+++ send_coordinates/update_coords.cpp
@@ -1,14 +1,17 @@
+
 // #include <iostream>
 // #include <unistd.h>
 // #include <vector>
 
-// using namespace std;
 
 
 
-void GettingCurrentCoordinates(vector<pair <int, int>>& node, vector<double>& times)
+// delay(5000); 
+
+
+void GettingCurrentCoordinates(std::vector<std::pair <double, double>>& node, std::vector<double>& times)
 {
-    vector<double> sum_time(1);
+    std::vector<double> sum_time(1);
     sum_time[0] = 0;
     for (int i = 0; i < times.size(); i++){
         sum_time.push_back(sum_time[i] + times[i]); // массив сумарного времени до каждой ноды
@@ -18,7 +21,7 @@ void GettingCurrentCoordinates(vector<pair <int, int>>& node, vector<double>& ti
     while (true){
         if (current_time >= sum_time[i]){
             //http запрос, отправляющий текущуюю координату в фронт (return node[i])
-            cout << node[i].first<<" "<<node[i].second<< '\n';
+            std::cout << node[i].first<<" "<<node[i].second<< '\n';
             i += 1;
         }
         sleep(60);
@@ -32,8 +35,8 @@ void GettingCurrentCoordinates(vector<pair <int, int>>& node, vector<double>& ti
 
 // int main()
 // {
-//     vector<pair <int, int>> a = {{1, 1}, {2, 2}, {5, 5}, {4, 4}, {3, 3}, {2, 2}, {7, 7}};
-//     vector<double> b = {118.2, 122, 131.22, 153, 142.2, 131.1};
+//     std::vector<std::pair <double, double>> a = {{1.1, 1.1}, {2.2, 2.2}, {5.5, 5.5}, {4.4, 4.4}, {3.3, 3.3}, {2.2, 2.2}, {7.7, 7.7}};
+//     std::vector<double> b = {118.2, 122, 131.22, 153, 142.2, 131.1};
 //     GettingCurrentCoordinates(a, b);
 
 //     return 0;
