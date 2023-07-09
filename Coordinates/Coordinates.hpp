e2f5f1efa289ed4516473c80aceddc6c23445e4d
ssanchellaa <113291093+ssanchellaa@users.noreply.github.com>
2023-07-09 12:28:14 +0300

    Add files via upload

---
+++ Coordinates/Coordinates.hpp
@@ -0,0 +1,34 @@
+#ifndef COORDINATES_HPP
+#define COORDINATES_HPP
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <fstream>
+
+class Elem
+{
+private:
+    double lat;
+    double lon;
+public:
+    Elem() = default;
+    ~Elem() = default;
+    friend class Coordinates;
+};
+
+class Coordinates 
+{
+private:
+    std::vector<Elem> coord;
+
+public:
+    Coordinates() = default;
+    ~Coordinates() = default;
+
+    void fillingCoord(const std::string &file);
+};
+
+#include "Coordinates.cpp"
+
+#endif //COORDINATES_HPP
