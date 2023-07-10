4ff1103a1ef8e7ec65219c78b3f95cadcc042947
KochevDmitry <65921394+KochevDmitry@users.noreply.github.com>
2023-07-10 18:08:10 +0300

    Update update_coords.cpp, add http responce

--- src/update_coords.cpp
+++ src/update_coords.cpp
@@ -1,6 +1,54 @@
 #include <iostream>
 #include <unistd.h>
 #include <vector>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+
+
+void sendHttpRequestWithCoords(double num1, double num2) {
+    // Создание io_context
+    boost::asio::io_context ioContext;
+
+    // Создание resolver
+    boost::asio::ip::tcp::resolver resolver(ioContext);
+
+    // Разрешение хоста и порта
+    auto const results = resolver.resolve("localhost", "8080");
+
+    // Создание сокета
+    boost::asio::ip::tcp::socket socket(ioContext);
+
+    // Установка соединения
+    boost::asio::connect(socket, results.begin(), results.end());
+
+    // Создание запроса
+    boost::beast::http::request<boost::beast::http::string_body> request;
+    request.method(boost::beast::http::verb::post);
+    request.target("/");
+    request.set(boost::beast::http::field::host, "localhost");
+    request.set(boost::beast::http::field::content_type, "application/json");
+
+    // Создание тела запроса с двумя дробными числами
+    std::string body = "{\"num1\": " + std::to_string(num1) + ", \"num2\": " + std::to_string(num2) + "}";
+    request.body() = std::move(body);
+
+    // Отправка запроса
+    boost::beast::http::write(socket, request);
+
+    // Получение и вывод ответа
+    // boost::beast::flat_buffer buffer;
+    // http::response<http::dynamic_body> response;
+    // http::read(socket, buffer, response);
+    // std::cout << "Ответ сервера: " << boost::beast::buffers_to_string(response.body().data()) << std::endl;
+
+    // Закрытие соединения
+    boost::system::error_code ec;
+    socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
+    if (ec && ec != boost::system::errc::not_connected) {
+        std::cerr << "Ошибка при закрытии соединения: " << ec.message() << std::endl;
+    }
+}
 
 
 void GettingCurrentCoordinates(std::vector<std::pair <double, double>>& node, std::vector<double>& times)
@@ -14,7 +62,7 @@ void GettingCurrentCoordinates(std::vector<std::pair <double, double>>& node, st
     int i = 0;
     while (true){
         if (current_time >= sum_time[i]){
-            //здесь будет http запрос, отправляющий текущуюю координату в фронт (return node[i])
+            sendHttpRequestWithCoords(node[i].first, node[i].second);
             std::cout << node[i].first<<" "<<node[i].second<< '\n';
             i += 1;
             if (i == sum_time.size())
